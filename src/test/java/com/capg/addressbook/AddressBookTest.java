8223066bbf92a2ecdb255bd7fdd9da03ba047b8b
ijounjalkar <71863485+ijounjalkar@users.noreply.github.com>
2020-11-06 00:18:37 +0530

    Added functionality to insert the data
    Added functionality to insert the data

--- src/test/java/com/capg/addressbook/AddressBookTest.java
+++ src/test/java/com/capg/addressbook/AddressBookTest.java
@@ -39,7 +39,7 @@ public class AddressBookTest {
 		boolean result = addressBookService.checkContactDataSync("Isha");
 		assertEquals(true, result);
 	}
-		/**
+         /**
 	 * Usecase18: retrieving data from the table between data range
 	 * 
 	 * @throws DatabaseException
@@ -54,7 +54,33 @@ public class AddressBookTest {
 		assertEquals(1, resultList.size());
 	}
 
+	/**
+	 * Usecase19: retrieving data from the table for city and state
+	 * 
+	 * @throws DatabaseException
+	 * @throws SQLException 
+	 */
 	@Test
 	public void givenContactInDB_WhenRetrievedForCityAndState_ShouldMatchContactCount() throws DatabaseException, SQLException {
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> contactData = addressBookService.readContactData(IOService.DB_IO);
+		List<Contact> resultList = addressBookService.getContactForCityAndState("Akola", "Maharashta");
+		assertEquals(2, resultList.size());
+	}
+
+	/**
+	 * Usecase20: Insert data into database in a single transaction
+	 * 
+	 * @throws DatabaseException
+	 * @throws SQLException
+	 */
+	@Test
+	public void givenContactInDB_WhenAdded_ShouldBeAddedInSingleTransaction() throws DatabaseException, SQLException {
+		AddressBookService addressBookService = new AddressBookService();
+		List<Contact> contactData = addressBookService.readContactData(IOService.DB_IO);
+		addressBookService.addContactInDatabase("Leena", "Sarode", "Panvel", "400019", "Mumbai", "Maharashtra",
+                                               "9859629542", "hjhgy@gmail.com", LocalDate.of(2021, 01, 01), "AddressBook1", "friend");
+		boolean result = addressBookService.checkContactDataSync("Leena");
+		assertEquals(true, result);
+	}
+	
