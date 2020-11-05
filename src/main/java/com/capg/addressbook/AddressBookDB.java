4e3f412b3cec9c8061fc225377faaaeedfaf834c
ijounjalkar <jounjalkarisha@gmail.com>
2020-11-05 23:28:55 +0530

    Added functionality to retrieve data between range

--- src/main/java/com/capg/addressbook/AddressBookDB.java
+++ src/main/java/com/capg/addressbook/AddressBookDB.java
@@ -1,14 +1,18 @@
 package com.capg.addressbook;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBookDB {
+	private static PreparedStatement contactStatement;
 	private static AddressBookDB addressBookDB;
 	
 	public AddressBookDB() {
@@ -42,29 +46,73 @@ public class AddressBookDB {
 		return this.getContactData(sql);
 	}
 
-	private List<Contact> getContactData(String sql) throws DatabaseException {
+	List<Contact> getContactData(String sql) throws DatabaseException {
 		List<Contact> contactList = new ArrayList<>();
 		try (Connection connection = this.getConnection()) {
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(sql);
-			while (resultSet.next() ) {
-				String fname = resultSet.getString("fname");
-				String lname = resultSet.getString("lname");
-				String address = resultSet.getString("address");
-				long zip = resultSet.getLong("zip");
-				String city = resultSet.getString("city");
-				String state = resultSet.getString("state");
-				long phoneNumber = resultSet.getLong("phone");
-				String email = resultSet.getString("email");
-				contactList.add(new Contact(fname,lname,address,state,city,zip,phoneNumber,email));
-				
-			}
+			contactList = this.getContactData(resultSet);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+		return contactList;			
+	}
+
+	public int updatePersonsData(String name, String phone) throws DatabaseException, SQLException {
+		return this.updatePersonsDataUsingStatement(name, phone);
+	}
+
+	private int updatePersonsDataUsingStatement(String name, String phone) throws DatabaseException, SQLException {
+		String sql = "Update contact_table set phone = ? where fname = ?";
+		int result = 0;
+		List<Contact> contactList = null;
+		if (this.contactStatement == null) {
+			Connection connection = this.getConnection();
+			contactStatement = connection.prepareStatement(sql);
+		}
+		try {
+			contactStatement.setLong(1, Long.parseLong(phone));
+			contactStatement.setString(2,name);
+			result = contactStatement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	private List<Contact> getContactData(ResultSet resultSet) throws SQLException {
+		List<Contact> contactList = new ArrayList<>();
+		while (resultSet.next()) {
+			String fname = resultSet.getString("fname");
+			String lname = resultSet.getString("lname");
+			String address = resultSet.getString("address");
+			long zip = resultSet.getLong("zip");
+			String city = resultSet.getString("city");
+			String state = resultSet.getString("state");
+			long phoneNumber = resultSet.getLong("phone");
+			String email = resultSet.getString("email");
+			contactList.add(new Contact(fname,lname,address,state,city,zip,phoneNumber,email));
+		}
 		return contactList;
 	}
-	
 
+	public List<Contact> getContactFromData(String name) throws DatabaseException {
+		String sql = String.format("SELECT * FROM contact_table WHERE fname = '%s'", name);
+		List<Contact> contactList = new ArrayList<>();
+		try (Connection connection = this.getConnection()) {
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(sql);
+			contactList = this.getContactData(resultSet);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;			
+	}
+	
+	public List<Contact> getEmployeeForDateRange(LocalDate start, LocalDate end) throws DatabaseException {
+		String sql = String.format(
+				"select contact_table.contact_id, contact_table.fname,contact_table.lname,contact_table.address,contact_table.zip, contact_table.city, contact_table.state, contact_table.phone,contact_table.email,contact_table.date, addressBook.addName, addressBook.type from contact_table inner join addressBook on contact_table.contact_id = addressBook.contacts_id where date between '%s' and '%s'",
+				Date.valueOf(start), Date.valueOf(end));
+		return this.getContactData(sql);
+	}
 }
--- src/test/java/com/capg/addressbook/AddressBookTest.java
+++ src/test/java/com/capg/addressbook/AddressBookTest.java
@@ -1,24 +1,60 @@
 package com.capg.addressbook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import org.junit.Test;
 
-import com.addressbookdb.AddressBookService.IOService;
-import com.addressbookdb.AddressBookService;
-import com.addressbookdb.Contact;
-import com.addressbookdb.DatabaseException;
+
+import com.capg.addressbook.AddressBookMain.IOService;
+import com.capg.addressbook.AddressBookService;
+import com.capg.addressbook.Contact;
+import com.capg.addressbook.DatabaseException;
+
+
 
 public class AddressBookTest {
 	/**
 	 * Usecase16: Retrieve data from the database
 	 * 
 	 * @throws DatabaseException
+	 * @throws SQLException 
 	 */
 	@Test
-	public void givenContactDataInDB_WhenRetrieved_ShouldMatchContactCount() throws DatabaseException {
+	public void givenContactDataInDB_WhenRetrieved_ShouldMatchContactCount() throws DatabaseException, SQLException {
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> contactData = addressBookService.readContactData(IOService.DB_IO);
 		assertEquals(4, contactData.size());
+	}
+	
+	/**Usecase17: Updating phone number of a persons in contact table
+	 * @throws DatabaseException
+	 * @throws SQLException
+	 */
+	@Test
+	public void givenNewDataForContact_WhenUpdated_ShouldBeInSync() throws DatabaseException, SQLException {
+		AddressBookService addressBookService = new AddressBookService();
+		List<Contact> contactData = addressBookService.readContactData(IOService.DB_IO);
+		addressBookService.updatePersonsPhone("Isha", "7457120752");
+		addressBookService.readContactData(IOService.DB_IO);
+		boolean result = addressBookService.checkContactDataSync("Isha");
+		assertEquals(true, result);
+	}
+	
+	/**
+	 * Usecase18: retrieving data from the table between data range
+	 * 
+	 * @throws DatabaseException
+	 * @throws SQLException 
+	 */
+	@Test
+	public void givenContactInDB_WhenRetrievedForDateRange_ShouldMatchContactCount() throws DatabaseException, SQLException {
+		AddressBookService addressBookService = new AddressBookService();
+		List<Contact> contactData = addressBookService.readContactData(IOService.DB_IO);
+		List<Contact> resultList = addressBookService.getContactForDateRange(LocalDate.of(2020, 01, 01),
+				LocalDate.of(2021, 01, 01));
+		assertEquals(1, resultList.size());
 	}
 }
